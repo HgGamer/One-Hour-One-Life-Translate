3ad8c6efb0a563b3fca29786a68c6b2a96f7faab
Mr.Catfood <sipos22@msn.com>
2018-03-17 20:25:09 +0100

    Collector js

---
+++ collect.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+fs.readdir('./objects', function(err, items) {
+    for (var i=0; i<items.length; i++) {
+		loadfile('./objects/'+items[i]).then((v2) => {
+			 console.log(v2);
+		});
+       
+    }
+});
+
+function loadfile(filename){
+		return new Promise((resolve, reject) => {
+		fs.readFile(filename, 'utf8', function(err, contents) {
+			
+			if(filename != "./objects/nextObjectNumber.txt" && filename != "./objects/cache.fcz"&& filename != "./objects/groundHeat_4.txt"){
+					resolve(filename.split("/")[2] + ";" + contents.split("\n")[1]);
+			}
+		});
+		})
+	
+	
+}
+
+
